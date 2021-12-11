d6f3ece1fac855081576cba142a85e19c285161f
Parth Verma <parthverma011@gmail.com>
2021-12-11 11:49:48 +0530

    Introduced uploader

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,6 @@ class Course < ApplicationRecord
   has_many :reviews
   has_many :order_items
   has_many :videos
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
   enum category: {Technology: "Technology", Development: "Development", Bussiness: "Bussiness", OfficeProductivity: "OfficeProductivity", PersonalDevelopment: "PersonalDevelopment", Marketing: "Marketing", Fitness:"Fitness"}
 end
